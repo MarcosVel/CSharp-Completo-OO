e9bd0e7ac42438b96f00163515ecfe6eeaba7745
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-15 19:43:19 -0300

    add aula 24 seção 3

---
+++ Secao03/Exerc_Proposto_24/Exerc_Proposto_24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_Proposto_24 {
+    class Program {
+        static void Main(string[] args) {
+            int codFunc, horas;
+            double valor, salario;
+
+            codFunc = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valor = double.Parse(Console.ReadLine());
+
+            salario = horas * valor;
+
+            Console.WriteLine($"NUMBER  = {codFunc}");
+            Console.WriteLine("SALARY = U$" + salario.ToString("f2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
