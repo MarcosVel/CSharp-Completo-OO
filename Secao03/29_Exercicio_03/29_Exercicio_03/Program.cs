67ab41fff0169077cc3e646df5598c951006dc9e
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-15 20:44:48 -0300

    add exerc 3 aula 29 seção 3

---
+++ Secao03/29_Exercicio_03/29_Exercicio_03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _29_Exercicio_03 {
+    class Program {
+        static void Main(string[] args) {
+            int a, b;            
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);            
+
+            if (a % b == 0 || b % a == 0) {
+                Console.WriteLine("Sao Multiplos");
+            } else {
+                Console.WriteLine("Nao sao multiplos");
+            }
+        }
+    }
+}
