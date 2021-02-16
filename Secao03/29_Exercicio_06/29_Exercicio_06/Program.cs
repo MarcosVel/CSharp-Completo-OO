da2196547affd8d2b3686fd99f8d5c803f337ec5
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-16 20:02:20 -0300

    29_Exercicio_06

---
+++ Secao03/29_Exercicio_06/29_Exercicio_06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _29_Exercicio_06 {
+    class Program {
+        static void Main(string[] args) {
+            double num;
+
+            num = double.Parse(Console.ReadLine());
+
+            if (num > 0 && num <= 25) {
+                Console.WriteLine("Intervalo [0,25]");
+            } else if (num > 25 && num <= 50) {
+                Console.WriteLine("Intervalo [25,50]");
+            } else if (num > 50 && num <= 75) {
+                Console.WriteLine("Intervalo [50,75]");
+            } else if (num > 75 && num <= 100) {
+                Console.WriteLine("Intervalo [75,100]");
+            } else {
+                Console.WriteLine("Fora de intervalo");
+            }
+        }
+    }
+}
