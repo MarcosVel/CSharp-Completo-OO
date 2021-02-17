dc0925115a88629c01892d71616903399c68e586
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-17 01:52:07 -0300

    33_Exercicio_02

---
+++ Secao03/33_Exercicio_02/33_Exercicio_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _33_Exercicio_02 {
+    class Program {
+        static void Main(string[] args) {
+            int X, Y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            X = int.Parse(vet[0]);
+            Y = int.Parse(vet[1]);
+
+            while (X != 0 || Y != 0) {
+                if (X > 0 && Y > 0) {
+                    Console.WriteLine("primeiro");
+                }
+                else if (X < 0 && Y > 0) {
+                    Console.WriteLine("segundo");
+                }
+                else if (X < 0 && Y < 0) {
+                    Console.WriteLine("terceiro");
+                }
+                else if (X > 0 && Y < 0) {
+                    Console.WriteLine("quarto");
+                }
+                vet = Console.ReadLine().Split(' ');
+                X = int.Parse(vet[0]);
+                Y = int.Parse(vet[1]);
+            }            
+        }
+    }
+}
