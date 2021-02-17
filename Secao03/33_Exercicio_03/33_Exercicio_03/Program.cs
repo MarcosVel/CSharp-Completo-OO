fba36b69bc789aefd6a2fb4603c33340b6368c2f
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-17 02:10:10 -0300

    33_Exercicio_03

---
+++ Secao03/33_Exercicio_03/33_Exercicio_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _33_Exercicio_03 {
+    class Program {
+        static void Main(string[] args) {
+            int cod, alcool = 0, gasolina = 0, diesel = 0;
+
+            cod = int.Parse(Console.ReadLine());
+
+            while (cod != 4) {
+                switch (cod) {
+                    case 1:
+                        alcool++;
+                        break;
+                    case 2:
+                        gasolina++;
+                        break;
+                    case 3:
+                        diesel++;
+                        break;
+                    default:
+                        break;
+                }
+                cod = int.Parse(Console.ReadLine());
+            }                
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine($"Alcool: {alcool}");
+            Console.WriteLine($"Gasolina: {gasolina}");
+            Console.WriteLine($"Diesel: {diesel}");
+        }
+    }
+}
