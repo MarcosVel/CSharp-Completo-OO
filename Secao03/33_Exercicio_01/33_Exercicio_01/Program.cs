86243b67cc19d46c9163b477547e4f5bc956bcd3
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-17 01:39:35 -0300

    33_Exercicio_01

---
+++ Secao03/33_Exercicio_01/33_Exercicio_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _33_Exercicio_01 {
+    class Program {
+        static void Main(string[] args) {
+            int senha;
+                       
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
