0e0141359d4b63a21a1d0a7392952c249d60a3b0
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-19 00:20:31 -0300

    35_Exercicio_07

---
+++ Secao03/35_Exercicio_07/35_Exercicio_07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _35_Exercicio_07 {
+    class Program {
+        static void Main(string[] args) {
+            int valor;
+
+            Console.Write("Entrada: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Saída:");
+
+            for (int i = 1; i <= x; i++) {
+                for (int j = 1; j <= 3; j++) {
+                    valor = (int)Math.Pow(i, j);
+                    Console.Write($"{valor} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
