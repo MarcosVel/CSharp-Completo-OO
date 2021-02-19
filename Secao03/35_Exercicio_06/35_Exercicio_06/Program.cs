ad222a3b95be0331ce748c83fff61226e7b03b47
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-18 23:27:44 -0300

    35_Exercicio_06

---
+++ Secao03/35_Exercicio_06/35_Exercicio_06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _35_Exercicio_06 {
+    class Program {
+        static void Main(string[] args) {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++) {
+                if (x % i == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
