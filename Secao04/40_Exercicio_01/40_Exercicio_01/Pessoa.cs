de4aa2b19550f0c57dd64f175c968cefbec2fc4e
MarcosVel <marcos.veloso@sempreceub.com>
2021-03-28 18:55:17 -0300

    add Seção 04

---
+++ Secao04/40_Exercicio_01/40_Exercicio_01/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _40_Exercicio_01 {
+    class Pessoa {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Secao04/40_Exercicio_01/40_Exercicio_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _40_Exercicio_01 {
+    class Program {
+        static void Main(string[] args) {
+            Pessoa a, b;
+
+            a = new Pessoa();
+            b = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            a.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            a.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            b.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            b.idade = int.Parse(Console.ReadLine());
+
+            if (a.idade > b.idade) {
+                Console.WriteLine("Pessoa mais velha: {0}", a.nome);
+            } else {
+                Console.WriteLine("Pessoa mais velha: {0}", b.nome);
+            }
+
+        }
+    }
+}
