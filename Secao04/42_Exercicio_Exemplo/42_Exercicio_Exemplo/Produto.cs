d5d305db34ca85a456c424f495a4d6d14a94c822
MarcosVel <marcos.veloso@sempreceub.com>
2021-03-31 23:01:07 -0300

    add parte 42_Exercicio

---
+++ Secao04/42_Exercicio_Exemplo/42_Exercicio_Exemplo/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _42_Exercicio_Exemplo {
+    class Produto {
+        public string nome;
+        public double preco;
+        public int quantidade;
+        public int attQtd;
+
+        // Adicionar ao estoque
+        public double AddEstoque() {
+            return quantidade += attQtd;
+        }
+
+        // Total
+        public double Total() {
+            return quantidade * preco;
+        }
+    }
+}
---
+++ Secao04/42_Exercicio_Exemplo/42_Exercicio_Exemplo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _42_Exercicio_Exemplo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre os dados do produto:");
+
+            Produto produto = new Produto();
+
+            Console.Write("Nome: ");
+            produto.nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            produto.quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // total
+            double total = produto.Total();
+            Console.WriteLine($"Dados do produto: {produto.nome}, $ {produto.preco:F2}, {produto.quantidade} unidades, Total: {total:F2}");
+
+            Console.WriteLine();
+
+            // leitura da quantidade a adicionar
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.attQtd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // quantidade atualizada
+            double qtdAtualizada = produto.AddEstoque();
+            total = produto.Total();
+            Console.WriteLine($"Dados atualizados: {produto.nome}, $ {produto.preco:F2}, {qtdAtualizada} unidades, Total: {total:F2}");
+
+        }
+    }
+}
