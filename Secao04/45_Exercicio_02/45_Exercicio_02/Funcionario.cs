36b900d5426a74966db3e593698abcaddea56c68
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-03 03:11:53 -0300

    45_Exercicio_02

---
+++ Secao04/45_Exercicio_02/45_Exercicio_02/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace _45_Exercicio_02 {
+    class Funcionario {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += (SalarioBruto / porcentagem);
+        }
+
+        public override string ToString() {
+            return $"{Nome}, $ {SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Secao04/45_Exercicio_02/45_Exercicio_02/Program.cs
+++ Secao04/45_Exercicio_02/45_Exercicio_02/Program.cs
@@ -3,7 +3,29 @@
 namespace _45_Exercicio_02 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+            f.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + f);
+
         }
     }
 }
