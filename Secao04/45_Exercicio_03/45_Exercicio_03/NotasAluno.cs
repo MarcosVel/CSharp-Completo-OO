e0e2c4185a094770ed064921fab7943ae5fc743d
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-03 15:59:05 -0300

    45_Exercicio_03

---
+++ Secao04/45_Exercicio_03/45_Exercicio_03/NotasAluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _45_Exercicio_03 {
+    class NotasAluno {
+        public string NomeAluno;
+        public double TotalNotas;
+        double media = 60.0;
+
+        public void NotaFinal(double nota) {
+            TotalNotas += nota;
+        }
+
+        public bool Aprovado() {
+            if (TotalNotas >= media) {
+                return true;
+            }
+            return false;
+        }
+        
+        public double PontosRestantes() {
+            if (TotalNotas < media) {
+                media -= TotalNotas;
+            }
+            return media;
+        }
+    }
+}
---
+++ Secao04/45_Exercicio_03/45_Exercicio_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace _45_Exercicio_03 {
+    class Program {
+        static void Main(string[] args) {
+            NotasAluno a = new NotasAluno();           
+
+            Console.Write("Nome do aluno: ");
+            a.NomeAluno = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+
+            for (int i = 0; i < 3; i++) {
+                double nota = double.Parse(Console.ReadLine());
+                a.NotaFinal(nota);
+            }
+
+            Console.WriteLine("NOTA FINAL = " + a.TotalNotas.ToString("F2", CultureInfo.InvariantCulture));
+
+            if(a.Aprovado() == true) {
+                Console.WriteLine("APROVADO");
+            } else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + a.PontosRestantes().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
