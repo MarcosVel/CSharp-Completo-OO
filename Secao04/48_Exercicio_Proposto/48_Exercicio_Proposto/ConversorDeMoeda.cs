93d0d119b5dbc2170461823ce43be4a8c8a6ad3a
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-03 18:54:20 -0300

    48_Exercicio_Proposto

---
+++ Secao04/48_Exercicio_Proposto/48_Exercicio_Proposto/ConversorDeMoeda.cs
@@ -0,0 +1,8 @@
+﻿namespace _48_Exercicio_Proposto {
+    class ConversorDeMoeda {
+        public static double Iof = 1.06; // 6%
+        public static double ValorSerPago(double cotacao, double valor) {
+            return (cotacao * valor) * Iof;
+        }
+    }
+}
---
+++ Secao04/48_Exercicio_Proposto/48_Exercicio_Proposto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _48_Exercicio_Proposto {
+    class Program {
+        static void Main(string[] args) {
+            double cotacao, valor, resultado;
+
+            Console.Write("Qual é a cotação do dólar? ");
+            cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares vc vai comprar? ");
+            valor = double.Parse(Console.ReadLine());
+
+            resultado = ConversorDeMoeda.ValorSerPago(cotacao, valor);
+
+            Console.WriteLine($"Valor a ser pago em reais = {resultado:F2}");
+        }
+    }
+}
